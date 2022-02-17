98eda02c0a990659d6fb3dd8a245e3d02addf85a
karthi-pt4878 <karthi-pt4878@karthi-pt4878.tsi.zohocorpin.com>
2022-02-17 15:26:36 +0530

    connection check

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotEnv = require('dotenv').config();
+const { sequelize } = require('./db_conn/db');
 
 
 const app = express();
@@ -8,6 +9,15 @@ const app = express();
 app.get('/', (req, res) => {
     res.send("Yes it's working");
 })
+
+
+sequelize.authenticate()
+    .then(() => {
+        console.log("DB connected.");
+    })
+    .catch(() => {
+        console.log("DB connection error");
+    })
 
 
 app.listen(process.env.PORT, () => {
