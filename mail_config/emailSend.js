f019bccf79d25583b4f1301a855e912cd4118ba3
karthi-pt4878 <karthi-pt4878@karthi-pt4878.tsi.zohocorpin.com>
2022-02-24 15:07:29 +0530

    mail configuration using sendgrid

--- mail_config/emailSend.js
+++ mail_config/emailSend.js
@@ -1,10 +1,8 @@
-const sgMail = require('@sendgrid/mail');
-const { visitorFields } = require('../export_logic/allModelFields');
-const { Parser } = require('json2csv');
-const { tempVisitors } = require('../temp_store/temporaryStore')
+const sgMail = require('@sendgrid/mail');  
+const pug = require('pug');
 
 
-module.exports.sendMail = function sendMail(message) {   
+function sendMail(message) {    
     sgMail.send(message)
         .then(response => {
             console.log("Email sent....");
@@ -16,20 +14,81 @@ module.exports.sendMail = function sendMail(message) {
         });
 }
 
-module.exports.setEmailBody = async function setEmailBody(sub, data) { 
-    let fields = visitorFields;
-    const json2csv = new Parser({ fields });
-    const csv = await json2csv.parse(data);
+// module.exports.setEmailBody = function setEmailBody(sub, data) { 
+//     let htmlTemplate;
+//     if (data.length > 0) { 
+//         htmlTemplate = `<table border=1 cellspacing=0 cellpadding=5>
+//         <tr>
+//             <th>Visitorid</th>
+//             <th>Name</th>
+//             <th>Email</th>
+//             <th>Phone</th>
+//             <th>Designation</th>
+//             <th>Purpose</th>
+//             <th>Organization</th>
+//             <th>City</th>
+//             <th>Check-in</th>
+//             <th>Check-out</th>
+//             <th>Other-info</th> 
+//         </tr> 
+//         `;
+//         data.forEach(d => {
+//             htmlTemplate += `<tr>
+//             <td>${d.dataValues.id}</td>
+//             <td>${d.dataValues.name}</td> 
+//             <td>${d.dataValues.email}</td> 
+//             <td>${d.dataValues.phone}</td> 
+//             <td>${d.dataValues.designation}</td> 
+//             <td>${d.dataValues.purpose}</td> 
+//             <td>${d.dataValues.organization}</td> 
+//             <td>${d.dataValues.city}</td>
+//             <td>${d.dataValues.check_in}</td> 
+//             <td>${d.dataValues.check_out}</td>`;
+//             let otherInfo = d.dataValues.other_info;
+//             let temp = "";
+//             for (let key in otherInfo) {
+//                 temp += key + ":" + otherInfo[key] + "; ";
+//             }
+//             htmlTemplate += `<td>${temp}</td></tr>`;
+//         });
+//         htmlTemplate += `</table>`;
+//     } else {
+//         htmlTemplate = "<div>There are no new visitors here...!!!</div>"
+//     }
+
+//     const message =  {
+//         to: ['karthikeyankn910@gmail.com', 'karthikeyan9102001@gmail.com'],
+//         from: {
+//             name: 'karthikeyan',
+//             email: 'karthikeyan9102001@gmail.com',
+//         },
+//         subject: "Visitor's report",
+//         html: htmlTemplate,
+//         text: "Here the visitors report"
+//     };     
+ 
+//     console.log(data.length);  
+//     sendMail(message); 
+// }
+
+
+
+
+module.exports.setEmailBody = function setEmailBody(sub, data) {  
+    console.log("a");
+    const htmls = pug.renderFile(__dirname+"/"+'template.pug', {data: data}); 
+    console.log("b");
     const message =  {
         to: ['karthikeyankn910@gmail.com', 'karthikeyan9102001@gmail.com'],
         from: {
             name: 'karthikeyan',
             email: 'karthikeyan9102001@gmail.com',
         },
-        subject: sub,
-        text: csv, 
-    };  
-    console.log(csv);  
-    tempVisitors = [];
-    return message;
+        subject: "Visitor's report",
+        html: htmls, 
+    };     
+
+    console.log(__dirname);
+  
+    sendMail(message); 
 }
