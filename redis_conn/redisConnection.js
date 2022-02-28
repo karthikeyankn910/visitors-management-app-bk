927f574f029013bbd42a357a55998fc8beff1e9e
karthi-pt4878 <karthi-pt4878@karthi-pt4878.tsi.zohocorpin.com>
2022-02-28 16:16:20 +0530

    redis configured

---
+++ redis_conn/redisConnection.js
@@ -0,0 +1,14 @@
+const redis = require('redis');
+
+
+
+const client = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+});
+
+client.on('connect', () => {
+    console.log("Redis Connected");
+});
+
+module.exports.client = client;
