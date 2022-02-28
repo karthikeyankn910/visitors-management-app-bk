49cc679b80b52647141bb749851fa9c6d5c730e2
karthi-pt4878 <karthi-pt4878@karthi-pt4878.tsi.zohocorpin.com>
2022-02-28 16:16:49 +0530

    modified for bull

--- export_logic/getData.js
+++ export_logic/getData.js
@@ -9,20 +9,25 @@ const { branchFields, employeeFields, visitorFields } = require('./allModelField
 
 
 
-const download = async (req, res, modelName) => {  
+const download = async (req, res, modelName, retrieveData) => {  
     if (modelName === 'branch') { 
         fields = branchFields;
-        data = await branchService.getAllBranches();
+        // data = await branchService.getAllBranches();
+        data = retrieveData;
         fileName = 'branch.csv';
     }
     else if (modelName === 'visitor') { 
         fields = visitorFields;
-        data = await visitorService.getAllVisitors();
+        // data = await visitorService.getAllVisitors();
+        data = retrieveData;
+
         fileName = 'visitor.csv';
     }
     else { 
         fields = employeeFields;
-        data = await employeeService.getAllEmployees();
+        // data = await employeeService.getAllEmployees();
+        data = retrieveData;
+
         fileName = 'employee.csv';
     } 
     return downloadResource(res, fileName, fields, data);  
